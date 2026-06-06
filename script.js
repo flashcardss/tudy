c1cbe292c78951f5312182bac9e72795a0500053
flashcardss <gencatyolanda@gmail.com>
2026-06-06 21:25:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+let flashcards = [];
+let current = 0;
+
+const questionEl = document.getElementById("question");
+const answerEl = document.getElementById("answer");
+const flashcardEl = document.getElementById("flashcard");
+
+const showBtn = document.getElementById("showBtn");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+const counterEl = document.getElementById("counter");
+
+fetch("flashcards.json?" + Date.now())
+    .then(response => response.json())
+    .then(data => {
+        flashcards = data;
+        showCard();
+    });
+
+function showCard() {
+
+    const card = flashcards[current];
+
+    questionEl.textContent = card.question;
+    answerEl.textContent = card.answer;
+
+    flashcardEl.classList.remove("flip");
+
+    counterEl.textContent =
+        `${current + 1} / ${flashcards.length}`;
+}
+
+showBtn.addEventListener("click", () => {
+
+    flashcardEl.classList.toggle("flip");
+
+});
+
+nextBtn.addEventListener("click", () => {
+
+    current++;
+
+    if (current >= flashcards.length) {
+        current = 0;
+    }
+
+    showCard();
+});
+
+prevBtn.addEventListener("click", () => {
+
+    current--;
+
+    if (current < 0) {
+        current = flashcards.length - 1;
+    }
+
+    showCard();
+});
